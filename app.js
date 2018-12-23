a87eaa53e1e6914699e7a3945ec139383993c12c
Paul Duvall <duvallpj@gmail.com>
2018-12-23 11:32:10 +0000

    Item constructor and data structure added to task controller, initialiser function added to App controller, set to run on initialisation

--- app.js
+++ app.js
@@ -1,32 +1,96 @@
+// *******************************************************************************
 // Storage controller
+// *******************************************************************************
 
 
+
+// *******************************************************************************
 // Task controller
+// *******************************************************************************
 
 const TaskCtrl = (function(){
   // Item constructor
-  const Task = function(id, task, stages){
-
+  const Task = function(id, title, stage1, stage2, stage3, priority){
+    this.title = title;
+    this.stage1 = stage1;
+    this.stage2 = stage2;
+    this.stage3 = stage3;
+    this.priority = priority;
   }
   
 
+  // Data structure / state
+  const data = {
+    items: [
+      {id: 0, title:'Create restaurant site', stage1:'Research other sites', stage2:'Source images', stage3:'Learn flexbox', priority:'High'},
+      {id: 1, title:'Learn React', stage1:'Complete Udemy Course', stage2:'Research hooks', stage3:'Select project', priority:'Medium'}
+    ],
+    // The task currently selected to be edited
+    currentTask: null,
+    // The stage currently selected to be added to current task tracking
+    currentStage: null,
+  }
+
+  return {
+    getItems: function(){
+      return data.items;
+    },
+    
+    logData: function(){
+      return data;
+    }
+  }
 
 })();
 
 
+
+// *******************************************************************************
 // UI controller
+// *******************************************************************************
 
 const UICtrl = (function(){
   
+  return {
+    populateTasks: function(tasks) {
+      html = '';
+
+      tasks.forEach((task) => {
+        console.log(task);
+        
+      });
+    }
+  }
 })();
 
 
+
+// *******************************************************************************
 // App controller
+// *******************************************************************************
 
 const App = (function(TaskCtrl, UICtrl){
-  
+  // console.log(TaskCtrl.logData());
+
+  return {
+    init: function(){
+      // Declare variable for list of tasls from data object 
+      const tasks = TaskCtrl.getItems();
+      
+      // Populate UI with tasks
+      UICtrl.populateTasks(tasks);
+    }
+  }
 })(TaskCtrl, UICtrl);
 
+
+
+// *******************************************************************************
+// End of app controller
+// *******************************************************************************
+
+// Initialise app
+App.init();
 
 
 // Get the modal
